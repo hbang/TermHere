8701b4516a2c13c5558e77fea96c89c50af5b0c6
Adam Demasi <adam@hbang.com.au>
2016-09-25 23:34:01 +0930

    [l10n] implement weird localization stuff

---
+++ TermHere/CocoaLocalization.swift
@@ -0,0 +1,36 @@
+//
+//  CocoaLocalization.swift
+//  TermHere
+//
+//  Created by Adam Demasi on 25/9/16.
+//  Copyright © 2016 HASHBANG Productions. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+func localize(_ key: String, table: String = "Localizable") -> String {
+	return NSLocalizedString(key, tableName: table, bundle: Bundle.main, value: "", comment: "")
+}
+
+extension NSTextField {
+
+	@IBInspectable var localizationTable: String {
+		get { return "" }
+		set {
+			cell!.title = localize(cell!.title, table: newValue)
+		}
+	}
+
+}
+
+extension NSButton {
+
+	@IBInspectable var localizationTable: String {
+		get { return "" }
+		set {
+			cell!.title = localize(cell!.title, table: newValue)
+		}
+	}
+
+}
