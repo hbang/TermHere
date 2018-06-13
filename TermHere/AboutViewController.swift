5839aae55eb909ea3fa8edbfa13265933a2d7ba7
Adam Demasi <adam@hbang.com.au>
2018-06-13 22:03:57 +0930

    [app] sort out UI and strings for the about box

--- TermHere/AboutViewController.swift
+++ TermHere/AboutViewController.swift
@@ -22,15 +22,46 @@ class AboutViewController: NSViewController {
 		let bundle = Bundle.main
 		let info = bundle.infoDictionary!
 
-		nameLabel.stringValue = "\(info["CFBundleName"]!) \(info["CFBundleShortVersionString"]!) (\(info["CFBundleVersion"]!))"
-		copyrightLabel.stringValue = info["NSHumanReadableCopyright"] as! String
+		nameLabel.stringValue = "\(info["CFBundleName"]!) \(info["CFBundleShortVersionString"]!)"
 
+		// construct the copyright/links label
+		let copyright = info["NSHumanReadableCopyright"] as! String
+
+		#if SANDBOX
+		let hbangURL = URL(string: "https://hbang.com.au/termhere/")!
+		let supportEmail = "support@hbang.ws"
+		#else
+		let hbangURL = URL(string: "https://hbang.ws/apps/termhere/")!
+		let supportEmail = "support@hbang.ws"
+		#endif
+		
+		var mailtoURL = URLComponents(string: "mailto:\(supportEmail)")!
+		mailtoURL.queryItems = [
+			URLQueryItem(name: "subject", value: "TermHere – Support")
+		]
+		
+		let paragraphStyle = NSMutableParagraphStyle()
+		paragraphStyle.alignment = .center
+		paragraphStyle.paragraphSpacing = 8
+
+		let attributedString = NSMutableAttributedString(string: "\(copyright)\n\(hbangURL.host!) — \(supportEmail)", attributes: [
+			.paragraphStyle: paragraphStyle,
+			.font: NSFont.systemFont(ofSize: NSFont.smallSystemFontSize),
+			.foregroundColor: NSColor.textColor
+		])
+		attributedString.addAttribute(.link, value: hbangURL, range: NSMakeRange(copyright.count + 1, hbangURL.host!.count))
+		attributedString.addAttribute(.link, value: mailtoURL.url!, range: NSMakeRange(copyright.count + 1 + hbangURL.host!.count + 3, supportEmail.count))
+		copyrightLabel.attributedStringValue = attributedString
+
+		// construct the credits text view
 		guard let data = try? Data(contentsOf: bundle.url(forResource: "Credits", withExtension: "html")!) else {
 			NSLog("whoa, the credits failed to load?")
 			return
 		}
-
-		textView.textStorage!.append(NSAttributedString(html: data, baseURL: bundle.resourceURL!, documentAttributes: nil)!)
+		
+		let creditsAttributedString = NSMutableAttributedString(html: data, baseURL: bundle.resourceURL!, documentAttributes: nil)!
+		creditsAttributedString.addAttribute(.foregroundColor, value: NSColor.textColor, range: NSMakeRange(0, creditsAttributedString.string.count))
+		textView.textStorage!.append(creditsAttributedString)
 	}
 
 	@IBAction func closeClicked(_ sender: AnyObject) {
